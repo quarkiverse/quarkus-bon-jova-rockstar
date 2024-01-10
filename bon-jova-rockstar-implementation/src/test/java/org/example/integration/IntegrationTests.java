4295de76bec363c60441fcfa510d0f363e34f58a
Holly Cummins <hcummins@redhat.com>
2024-01-10 20:49:27 +0000

    Add 99 bottles of beer to integration tests (yay!)

---
+++ bon-jova-rockstar-implementation/src/test/java/org/example/integration/IntegrationTests.java
@@ -0,0 +1,29 @@
+package org.example.integration;
+
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.example.util.FileLauncher.compileAndLaunch;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class IntegrationTests {
+
+    @Test
+    public void shouldRun99BottlesOfBeer() throws IOException, URISyntaxException {
+        String output = compileAndLaunch("/99-bottles.rock");
+        String expected = getFileContents("/expected-99-bottles.output");
+        assertEquals(expected, output);
+    }
+
+    private String getFileContents(String name) throws URISyntaxException, IOException {
+        Path filePath = Path.of(this.getClass()
+                                    .getResource(name)
+                                    .toURI());
+
+        return Files.readString(filePath);
+    }
+}
