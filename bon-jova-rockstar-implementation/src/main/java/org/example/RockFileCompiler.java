95be9c1682e289df095b5dccd6cf03aa985304aa
Holly Cummins <hcummins@redhat.com>
2024-01-15 19:53:29 +0000

    Allow compiler to be invoked as java app (and remove deeply confusing output)

--- bon-jova-rockstar-implementation/src/main/java/org/example/RockFileCompiler.java
+++ bon-jova-rockstar-implementation/src/main/java/org/example/RockFileCompiler.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -8,8 +10,18 @@ public class RockFileCompiler {
     public static final String DOT_CLASS = ".class";
     public static final String DOT_ROCK = ".rock";
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws FileNotFoundException {
+        String fileName = args[0];
+        if (fileName == null) {
+            System.out.println("Usage: first argument should be the path to a .rock file");
+        }
+        try (InputStream stream = new FileInputStream(fileName)) {
+            File outFile = new File(fileName.replace(DOT_ROCK, DOT_CLASS));
+            System.out.println("Compiling " + fileName + " to " + outFile);
+            new RockFileCompiler().compile(stream, outFile);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void compile(InputStream stream, File outFile) throws IOException {
