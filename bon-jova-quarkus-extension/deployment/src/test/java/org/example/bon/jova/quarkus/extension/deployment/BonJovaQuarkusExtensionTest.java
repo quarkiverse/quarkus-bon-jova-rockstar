f1629ffbc739f1534ef0266addf71c2676008a44
Hanno Embregts <hanno.embregts@infosupport.com>
2024-01-16 15:27:14 +0100

    Read query params on rest endpoint and pipe to rockstar's stdin

--- bon-jova-quarkus-extension/deployment/src/test/java/org/example/bon/jova/quarkus/extension/deployment/BonJovaQuarkusExtensionTest.java
+++ bon-jova-quarkus-extension/deployment/src/test/java/org/example/bon/jova/quarkus/extension/deployment/BonJovaQuarkusExtensionTest.java
@@ -2,6 +2,7 @@ package org.example.bon.jova.quarkus.extension.deployment;
 
 import io.quarkus.test.QuarkusUnitTest;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
@@ -13,9 +14,18 @@ public class BonJovaQuarkusExtensionTest {
             .withEmptyApplication();
 
     @Test
-    public void testRockstarEndpoint() {
+    void testRockstarEndpoint() {
         RestAssured.when().get("/rockstar/hello_world.rock")
                 .then().statusCode(200)
                 .and().body(containsString("Hello World"));
+    }
+
+    @Test
+    @Disabled("until support for 'reading from stdin' is added to the compiler")
+    void testRockstarProgramWithArguments() {
+        RestAssured.when().get("/rockstar/hello_hanno_hello_holly.rock?arg=Hanno&arg=Holly")
+                .then().statusCode(200)
+                .and().body(containsString("Hello Hanno"))
+                .and().body(containsString("Hello Holly"));
     }
 }
--- bon-jova-quarkus-extension/runtime/src/main/java/org/example/bon/jova/quarkus/extension/runtime/RockstarResource.java
+++ bon-jova-quarkus-extension/runtime/src/main/java/org/example/bon/jova/quarkus/extension/runtime/RockstarResource.java
@@ -3,13 +3,17 @@ package org.example.bon.jova.quarkus.extension.runtime;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
 import org.apache.commons.io.FilenameUtils;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.List;
 
 import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
 
@@ -17,17 +21,17 @@ import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
 public class RockstarResource {
     @GET
     @Path("/{programName}")
-    public Response runRockstarProgram(@PathParam("programName") String programName) {
+    public Response runRockstarProgram(@PathParam("programName") String programName, @QueryParam("arg") List<String> args) {
         try {
-            // We support both "hello-world" and "hello-world.rock".
-            String output = runClassCapturingSystemOut(FilenameUtils.removeExtension(programName));
+            // We support both "hello-world" and "hello-world.rock" as 'programName'.
+            String output = runClassCapturingSystemOut(FilenameUtils.removeExtension(programName), args.toArray(new String[0]));
             return Response.ok().entity(output).build();
         } catch (ClassNotFoundException | NoSuchMethodException | InvocationTargetException | IllegalAccessException e) {
             return Response.status(NOT_FOUND).entity(e).build();
         }
     }
 
-    private String runClassCapturingSystemOut(String className) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
+    private String runClassCapturingSystemOut(String className, String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException, ClassNotFoundException {
         var baos = new ByteArrayOutputStream();
         var ps = new PrintStream(baos);
 
@@ -37,11 +41,7 @@ public class RockstarResource {
         Class<?> rockstarClass = Class.forName(className, true, Thread.currentThread().getContextClassLoader());
         Method main = rockstarClass.getMethod("main", String[].class);
 
-        // TODO: support passing actual arguments via the REST endpoint.
-        final Object[] args = new Object[1];
-        args[0] = new String[] { "1", "2"};
-
-        main.invoke(null, args);
+        main.invoke(null, new Object[]{args});
 
         System.out.flush();
         System.setOut(systemOut);
