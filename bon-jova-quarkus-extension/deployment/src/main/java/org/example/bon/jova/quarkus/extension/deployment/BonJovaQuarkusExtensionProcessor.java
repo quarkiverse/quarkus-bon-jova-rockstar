7cf1c4a92f23e81c375fae79d16612553715d8b2
Hanno Embregts <hanno.embregts@infosupport.com>
2024-01-17 21:07:48 +0100

    Add menu option 'Rockstar Endpoints' that leads to a full page

--- bon-jova-quarkus-extension/deployment/src/main/java/org/example/bon/jova/quarkus/extension/deployment/BonJovaQuarkusExtensionProcessor.java
+++ bon-jova-quarkus-extension/deployment/src/main/java/org/example/bon/jova/quarkus/extension/deployment/BonJovaQuarkusExtensionProcessor.java
@@ -3,26 +3,32 @@ package org.example.bon.jova.quarkus.extension.deployment;
 import io.quarkus.bootstrap.model.ApplicationModel;
 import io.quarkus.bootstrap.workspace.SourceDir;
 import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.deployment.IsDevelopment;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.AdditionalClassLoaderResourcesBuildItem;
 import io.quarkus.deployment.builditem.AdditionalIndexedClassesBuildItem;
 import io.quarkus.deployment.builditem.FeatureBuildItem;
 import io.quarkus.deployment.pkg.builditem.CurateOutcomeBuildItem;
+import io.quarkus.devui.spi.page.CardPageBuildItem;
+import io.quarkus.devui.spi.page.Page;
 import org.example.RockFileCompiler;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.example.RockFileCompiler.DOT_CLASS;
 import static org.example.RockFileCompiler.DOT_ROCK;
+
 import org.example.bon.jova.quarkus.extension.runtime.RockstarResource;
 
 class BonJovaQuarkusExtensionProcessor {
@@ -46,17 +52,17 @@ class BonJovaQuarkusExtensionProcessor {
         ApplicationModel model = curateOutcomeBuildItem.getApplicationModel();
 
         if (model.getAppArtifact()
-                 .getWorkspaceModule() != null) {
+                .getWorkspaceModule() != null) {
 
             Collection<SourceDir> sourceDirs = model.getAppArtifact()
-                                                    .getSources()
-                                                    .getSourceDirs();
+                    .getSources()
+                    .getSourceDirs();
 
 
             String[] allCompiledFiles = sourceDirs.stream()
-                                                  .flatMap(this::compileEverythingInDir)
-                                                  .collect(Collectors.toList())
-                                                  .toArray(String[]::new);
+                    .flatMap(this::compileEverythingInDir)
+                    .collect(Collectors.toList())
+                    .toArray(String[]::new);
 
             // Try and add it to the additional classes to index
             // This seems like a reasonable idea, but it fails noisily with null streams, and causes an infinite loop; not doing it has
@@ -72,12 +78,12 @@ class BonJovaQuarkusExtensionProcessor {
         try {
             return Files.list(sourceDir.getDir())
 
-                        .filter(file -> file.toString()
-                                            .endsWith(DOT_ROCK))
-                        .map(java.nio.file.Path::toFile)
-                        .map(file -> compile(sourceDir.getOutputDir(),
-                                file, compiler))
-                        .map(file -> file.getAbsolutePath());
+                    .filter(file -> file.toString()
+                            .endsWith(DOT_ROCK))
+                    .map(java.nio.file.Path::toFile)
+                    .map(file -> compile(sourceDir.getOutputDir(),
+                            file, compiler))
+                    .map(file -> file.getAbsolutePath());
 
             // compiler for the filter, instead
         } catch (IOException e) {
@@ -88,7 +94,7 @@ class BonJovaQuarkusExtensionProcessor {
 
     private File compile(Path buildDir, File file, RockFileCompiler compiler) {
         String outFilename = file.getName()
-                                 .replace(DOT_ROCK, DOT_CLASS);
+                .replace(DOT_ROCK, DOT_CLASS);
         File outFile = new File(buildDir.toFile(), outFilename);
 
         // Check if the file is up to date before compiling, or the compile will trigger a restart, which triggers a compile, which
@@ -113,5 +119,49 @@ class BonJovaQuarkusExtensionProcessor {
     @BuildStep
     void addRockstarResource(BuildProducer<AdditionalBeanBuildItem> additionalBeans) {
         additionalBeans.produce(AdditionalBeanBuildItem.unremovableOf(RockstarResource.class));
+    }
+
+    @BuildStep(onlyIf = IsDevelopment.class)
+    public CardPageBuildItem pages() {
+        var cardPageBuildItem = new CardPageBuildItem();
+        List<RockFileViewModel> rockFiles = generateRockFiles();
+        cardPageBuildItem.addBuildTimeData("rockFiles", rockFiles);
+        cardPageBuildItem.addPage(Page.webComponentPageBuilder()
+                .icon("font-awesome-regular:address-book")
+                .componentLink("qwc-bon-jova-rockstar-endpoints.js")
+                .title("Rockstar Endpoints")
+                .staticLabel(String.valueOf(rockFiles.size())));
+
+        return cardPageBuildItem;
+    }
+
+    private List<RockFileViewModel> generateRockFiles() {
+        try (var stream = Files.list(Path.of("src/main/rockstar"))) {
+            return stream.map(path -> {
+                var fileName = path.getFileName().toString();
+                String contents;
+                try {
+                    contents = Files.readAllLines(path).stream().collect(Collectors.joining(System.lineSeparator()));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
+                return new RockFileViewModel(fileName, contents, generateRestUrl(fileName), calculateRockScore(contents));
+            }).toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private String generateRestUrl(String rockFileName) {
+        return "/rockstar/" + rockFileName;
+    }
+
+    private int calculateRockScore(String contents) {
+        // TODO: calculate a more sophisticated rock store
+        return Math.min(100, contents.length());
+    }
+
+    record RockFileViewModel(String name, String contents, String restUrl, int rockScore) {
     }
 }
