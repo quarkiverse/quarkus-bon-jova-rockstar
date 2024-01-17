e6505851f4e7f054fa38a5ada9d4b78033fd3d39
Hanno Embregts <hanno.embregts@infosupport.com>
2024-01-17 21:07:48 +0100

    Add menu option 'Rockstar Endpoints' that leads to a full page

---
+++ bon-jova-quarkus-extension/deployment/src/main/resources/dev-ui/qwc-bon-jova-rockstar-endpoints.js
@@ -0,0 +1,30 @@
+import { LitElement, html, css} from 'lit';
+import { rockFiles } from 'build-time-data';
+
+/**
+ * This component shows the Rockstar endpoints.
+ */
+export class QwcBonJovaRockstarEndpoints extends LitElement {
+
+    constructor() {
+        super();
+        this._rockFiles = rockFiles;
+    }
+
+    render() {
+        if (this._rockFiles) {
+            return html`<ul>
+                ${this._rockFiles.map((rockFile) =>
+                html`<li>${rockFile.name}</li>
+                        <ul>
+                            <li>${rockFile.contents}</li>
+                            <li>score: ${rockFile.rockScore}</li>
+                            <li><a href="${rockFile.restUrl}" target="_blank">call endpoint</a></li>
+                        </ul>`
+            )}</ul>`;
+        } else {
+            return html`No Rockstar endpoints found`;
+        }
+    }
+}
+customElements.define('qwc-bon-jova-rockstar-endpoints', QwcBonJovaRockstarEndpoints);
