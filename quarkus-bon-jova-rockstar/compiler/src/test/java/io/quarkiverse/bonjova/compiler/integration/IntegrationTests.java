76c5da160c9dcedbab722994c1d7773a22cb3ac1
Hanno Embregts <hanno.embregts@infosupport.com>
2024-02-05 17:01:10 +0100

    Add boring concept programs, reorganise the test class

--- quarkus-bon-jova-rockstar/compiler/src/test/java/io/quarkiverse/bonjova/compiler/integration/IntegrationTests.java
+++ quarkus-bon-jova-rockstar/compiler/src/test/java/io/quarkiverse/bonjova/compiler/integration/IntegrationTests.java
@@ -3,115 +3,61 @@ package io.quarkiverse.bonjova.compiler.integration;
 import io.quarkiverse.bonjova.compiler.util.FileLauncher;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class IntegrationTests {
+class IntegrationTests {
+    @ParameterizedTest
+    @MethodSource("provideTestData")
+    void runningRockstarProgramShouldYieldExpectedOutput(String inputFile, String rockstarFile,
+                                                         String expectedOutputFile) throws IOException, URISyntaxException {
+        String output;
 
-    @Test
-    public void shouldRun99BottlesOfBeer() throws IOException, URISyntaxException {
-        String output = FileLauncher.compileAndLaunch("/99-bottles.rock");
-        String expected = getFileContents("/expected-99-bottles.output");
+        if (inputFile != null) {
+            output = FileLauncher.compileAndLaunch(rockstarFile, getFileContents(inputFile));
+        } else {
+            output = FileLauncher.compileAndLaunch(rockstarFile);
+        }
+
+        String expected = getFileContents(expectedOutputFile);
         assertEquals(expected, output);
     }
 
-    @Test
-    public void shouldRunFizzBuzz() throws IOException, URISyntaxException {
-        String output = FileLauncher.compileAndLaunch("/fizzbuzz.rock");
-        String expected = getFileContents("/expected-fizzbuzz.output");
-        assertEquals(expected, output);
-    }
-
-    @Disabled("Not yet working")
-    @Test
-    public void shouldRunFactorization() throws IOException, URISyntaxException {
-        String output = FileLauncher.compileAndLaunch("/factorization.rock", "178");
-        assertEquals("2\n89\n", output);
-    }
-
-    @Test
-    public void shouldRunMandelbrot() throws IOException, URISyntaxException {
-        String output = FileLauncher.compileAndLaunch("/mandelbrot.rock");
-        String expected = getFileContents("/expected-mandelbrot.output");
-        assertEquals(expected, output);
-    }
-
-    @Test
-    public void shouldRunBoringMandelbrot() throws IOException, URISyntaxException {
-        String output = FileLauncher.compileAndLaunch("/mandelbrot-boring.rock");
-        String expected = getFileContents("/expected-mandelbrot.output");
-        assertEquals(expected, output);
-    }
-
-    @Disabled("Not yet working")
-    @Test
-    public void shouldRunOneBillionRowChallenge() throws IOException, URISyntaxException {
-        String input = getFileContents("/1brc.input");
-        String output = FileLauncher.compileAndLaunch("/1brc.rock", input);
-        String expected = getFileContents("/1brc.output");
-        assertEquals(expected, output);
-    }
-
-    @Test
-    void shouldRunSingIt() throws IOException, URISyntaxException {
-        String output = FileLauncher.compileAndLaunch("/concept-demo-1-sing-it.rock");
-        String expected = getFileContents("/expected-concept-demo-1-sing-it.output");
-        assertEquals(expected, output);
-    }
-
-    @Test
-    void shouldRunTheTideIsLow() throws IOException, URISyntaxException {
-        String output = FileLauncher.compileAndLaunch("/concept-demo-2-the-tide-is-low.rock").replace(',', '.');
-        String expected = getFileContents("/expected-concept-demo-2-the-tide-is-low.output");
-        assertEquals(expected, output);
-    }
-
-    @Test
-    void shouldRunOpen() throws IOException, URISyntaxException {
-        String output = FileLauncher.compileAndLaunch("/concept-demo-3-open.rock");
-        String expected = getFileContents("/expected-concept-demo-3-open.output");
-        assertEquals(expected, output);
-    }
-
-    @Test
-    void shouldRunCityWalls() throws IOException, URISyntaxException {
-        String output = FileLauncher.compileAndLaunch("/concept-demo-4-city-walls.rock");
-        String expected = getFileContents("/expected-concept-demo-4-city-walls.output");
-        assertEquals(expected, output);
-    }
-
-    @Test
-    void shouldRunTrustworthyFriend() throws IOException, URISyntaxException {
-        String output = FileLauncher.compileAndLaunch("/concept-demo-5-trustworthy-friend.rock");
-        String expected = getFileContents("/expected-concept-demo-5-trustworthy-friend.output");
-        assertEquals(expected, output);
-    }
-
-    @Test
-    void shouldRunMamaTakingCharge() throws IOException, URISyntaxException {
-        String output = FileLauncher.compileAndLaunch("/concept-demo-6-mama-taking-charge.rock");
-        String expected = getFileContents("/expected-concept-demo-6-mama-taking-charge.output");
-        assertEquals(expected, output);
+    private static Stream<Arguments> provideTestData() {
+        return Stream.of(
+                Arguments.of(null, "/99-bottles.rock", "/expected-99-bottles.output"),
+                Arguments.of(null, "/fizzbuzz.rock", "/expected-fizzbuzz.output"),
+                // not yet working: Arguments.of("/factorization.rock", "178"),
+                Arguments.of(null, "/mandelbrot.rock", "/expected-mandelbrot.output"),
+                Arguments.of(null, "/mandelbrot-boring.rock", "/expected-mandelbrot.output"),
+                // not yet working: Arguments.of("/1brc.input", "/1brc.rock", "/1brc.output"),
+                Arguments.of(null, "/concept-demo-1-sing-it.rock", "/expected-concept-demo-1-sing-it.output"),
+                Arguments.of(null, "/concept-demo-1-boring.rock", "/expected-concept-demo-1-sing-it.output"),
+                Arguments.of(null, "/concept-demo-2-the-tide-is-low.rock", "/expected-concept-demo-2-the-tide-is-low.output"),
+                Arguments.of(null, "/concept-demo-2-boring.rock", "/expected-concept-demo-2-the-tide-is-low.output"),
+                Arguments.of("/concept-demo-3-open.input", "/concept-demo-3-open.rock", "/expected-concept-demo-3-open.output"),
+                Arguments.of("/concept-demo-3-open.input", "/concept-demo-3-boring.rock", "/expected-concept-demo-3-open.output"),
+                Arguments.of(null, "/concept-demo-4-city-walls.rock", "/expected-concept-demo-4-city-walls.output"),
+                Arguments.of(null, "/concept-demo-4-boring.rock", "/expected-concept-demo-4-city-walls.output"),
+                Arguments.of(null, "/concept-demo-5-trustworthy-friend.rock", "/expected-concept-demo-5-trustworthy-friend.output"),
+                Arguments.of(null, "/concept-demo-5-boring.rock", "/expected-concept-demo-5-trustworthy-friend.output"),
+                Arguments.of(null, "/concept-demo-6-mama-taking-charge.rock", "/expected-concept-demo-6-mama-taking-charge.output"),
+                Arguments.of(null, "/concept-demo-6-boring.rock", "/expected-concept-demo-6-mama-taking-charge.output")
+        );
     }
 
     private String getFileContents(String name) throws URISyntaxException, IOException {
-        Path filePath = Path.of(this.getClass()
-                .getResource(name)
-                .toURI());
+        Path filePath = Path.of(this.getClass().getResource(name).toURI());
 
         return Files.readString(filePath);
-    }
-
-    private String[] getFileContentsAsArray(String name) throws URISyntaxException, IOException {
-        Path filePath = Path.of(this.getClass()
-                .getResource(name)
-                .toURI());
-
-        return Files.readAllLines(filePath).toArray(new String[0]);
     }
 }
